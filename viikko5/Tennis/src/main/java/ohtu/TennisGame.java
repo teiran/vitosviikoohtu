c3df98cbcb2b020e2536446fa6885d9890e7ab4d
teiran <tiera.hirvonen@gmail.com>
2017-04-24 20:26:19 +0300

    tehtavä5

--- viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,7 +1,7 @@
 package ohtu;
 
 public class TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -13,67 +13,56 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName == "player1") {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+        if (m_score1 == m_score2) {
+            if (0 <= m_score1 && 3 >= m_score1) {
+                score = cases(m_score1) + "-All";
+            } else {
+                score = "Deuce";
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            score = cases2(m_score1 - m_score2);
+            if (m_score1 > m_score2) {
+                score += "player1";
+            } else {
+                score += "player2";
             }
+        } else {
+            score += cases(m_score1) + "-" + cases(m_score2);
+        }
+        return score;
+    }
+
+    private String cases2(int minusResult) {
+        if (minusResult == 1 || minusResult == -1) {
+            return "Advantage ";
+        }
+        return "Win for ";
+    }
+
+    private String cases(int tempScore) {
+        String score = "";
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
         }
         return score;
     }
